03bbf2a2bb44f19fb50801cdd3331b51a0b95193
geraldanekwe <gerald.anekwe.ii@gmail.com>
2015-07-24 10:57:44 -0700

    first commit

---
+++ 001/script.jsx
@@ -0,0 +1,28 @@
+var Hello = React.createClass({
+  render: function() {
+    return <h1>Hello from Component</h1>;
+  }
+});
+
+var Goodbye = React.createClass({
+  render: function() {
+    var greetings = ["Hello", "Hi", "Welcome"];
+    var names = ["Gerald", "Dan", "Trey", "Ryan", "dude", "person", "maybe", "endless"];
+    var hellos = [];
+
+    for(var i = 0; i < names.length; i++){
+      hellos.push(<h1>{greetings[i%3]} {names[i]}</h1>)
+    };
+
+    return (
+      <div>
+        {hellos}
+      </div>
+    );
+  }
+})
+
+React.render(
+<Goodbye />,
+document.getElementById("root")
+);
