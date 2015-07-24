7b7c52d4e8b572cbe05c2ffcbac557cdd00a4f32
geraldanekwe <gerald.anekwe.ii@gmail.com>
2015-07-24 11:53:07 -0700

    more basics

--- 002/script.jsx
+++ 002/script.jsx
@@ -8,7 +8,26 @@ var Hello = React.createClass({
   }
 })
 
+var App = React.createClass({
+  render: function() {
+
+    var component = this, collection = component.props.collection;
+
+    var hellos = Object.keys(collection).map(function(name){
+      return <Hello key={name} name={name} greeting={collection[name]} />
+    });
+
+    return (
+      <div>
+        <h1>React Example</h1>
+        {hellos}
+      </div>
+    )
+  }
+});
+
+
 React.render(
-<Hello name="Ryan" greeting="Hello" />,
+<App collection={ {gerald: 'hi', ryan: 'hello', trey: 'yo'} }/>,
 document.getElementById("root")
 );
